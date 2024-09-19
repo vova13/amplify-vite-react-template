b0537329aff1f5195d4c877bd96b2dbd79dd123a
vova13 <vova13@hotmail.com>
2024-09-19 00:41:32 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+
 const client = generateClient<Schema>();
 
 function App() {
@@ -18,6 +21,10 @@ function App() {
   }
 
   return (
+        
+    <Authenticator>
+      {({ signOut }) => (
+      
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
@@ -27,13 +34,18 @@ function App() {
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo. Vova here .....
+        🥳 App successfully hosted. Try creating a new todo.
         <br />
         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
           Review next step of this tutorial.
         </a>
       </div>
+
+                <button onClick={signOut}>Sign out</button>
     </main>
+          
+      )}
+    </Authenticator>
   );
 }
 
